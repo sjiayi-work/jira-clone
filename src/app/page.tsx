0bc2f6fb6c3474aeae9749b9ea5470b98cb2a959
jiayi <sjiayi.work@gmail.com>
2025-04-07 22:12:27 +0800

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+    return (
+        <div>
+            <Button variant="ghost" size="lg">Click me!</Button>
+            <p className="text-red-500 font-semibold">
+                Antonio
+            </p>
+        </div>
+    );
+};
