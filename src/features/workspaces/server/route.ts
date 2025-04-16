63be22882403b1c2b1cb86888bbde017a6a9e2cc
jiayi <sjiayi.work@gmail.com>
2025-04-16 11:48:52 +0800

    JC-16: Add reset invite functionality
    - Defined new route: `POST /api/workspaces/:workspaceId/reset-invite-code`
    - Created `useResetInviteCode` hook to trigger the REST API and handles the response returned.
    - Added a "Reset Invite Code" zone in the `<EditWorkspaceForm>` component.

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -10,6 +10,8 @@ import { generateInviteCode } from '@/lib/utils';
 
 import { createWorkspaceSchema, updateWorkspaceSchema } from '../schema';
 
+const INVITE_CODE_LENGTH = 6;
+
 const app = new Hono()
     // JC-7: Create a workspace
     .post('/', zValidator('form', createWorkspaceSchema), sessionMiddleware, async (c) => {
@@ -32,7 +34,7 @@ const app = new Hono()
             name, 
             userId: user.$id,
             imageUrl: uploadedImageUrl,
-            inviteCode: generateInviteCode(6)
+            inviteCode: generateInviteCode(INVITE_CODE_LENGTH)
         });
         
         // JC-10: Create member
@@ -111,6 +113,24 @@ const app = new Hono()
         await databases.deleteDocument(DATABASE_ID, WORKSPACES_ID, workspaceId);
         
         return c.json({ data: { $id: workspaceId } });
-    });
+    })
+    // JC-16: Update workspace invite code
+    .post('/:workspaceId/reset-invite-code', sessionMiddleware, async (c) => {
+        const databases = c.get('databases');
+        const user = c.get('user');
+        
+        const { workspaceId } = c.req.param();
+        
+        const member = await getMember({ databases, workspaceId, userId: user.$id });
+        if (!member || member.role !== MemberRole.ADMIN) {
+            return c.json({ error: 'Unauthorized' }, 401);
+        }
+        
+        const workspace = await databases.updateDocument(DATABASE_ID, WORKSPACES_ID, workspaceId, {
+            inviteCode: generateInviteCode(INVITE_CODE_LENGTH)
+        });
+        
+        return c.json({ data: workspace });
+    });;
 
 export default app;
