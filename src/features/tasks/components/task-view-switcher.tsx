1069634b35461af5b8df05a989e820247260a024
jiayi <sjiayi.work@gmail.com>
2025-04-17 14:38:58 +0800

    JC-21: Build task APIs
    - Configured `NEXT_PUBLIC_APPWRITE_TASKS_ID` in `.env`
    - Added REST APIs:
      - **`POST /api/tasks`** - create a task
      - **`GET /api/taask`** - list all tasks in a project
    - Adedd schema **`createTaskSchema`** to be used by `POST /api/tasks`
    - Added server components: **TaskViewSwitcher**
    - Added **tasks** collection into `database.puml`

---
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -0,0 +1,57 @@
+import { PlusIcon } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import { DottedSeparator } from '@/components/dotted-separator';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
+/**
+ * JC-21: Task view with multiple tabls.
+ * 
+ * @example <TaskViewSwitcher />
+ */
+
+export const TaskViewSwitcher = () => {
+    return (
+        <Tabs className="flex-1 w-full border rounded-lg">
+            <div className="h-full flex flex-col overflow-auto p-4">
+                <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
+                    <TabsList className="w-full lg:w-auto">
+                        <TabsTrigger className="h-8 w-full lg:w-auto" value="table">
+                            Table
+                        </TabsTrigger>
+                        
+                        <TabsTrigger className="h-8 w-full lg:w-auto" value="kanban">
+                            Kanban
+                        </TabsTrigger>
+                        
+                        <TabsTrigger className="h-8 w-full lg:w-auto" value="calendar">
+                            Calendar
+                        </TabsTrigger>
+                    </TabsList>
+                    
+                    <Button className="w-full lg:w-auto" size="sm">
+                        <PlusIcon className="size-4 mr-2" />New
+                    </Button>
+                </div>
+                
+                <DottedSeparator className="my-4" />
+                
+                {/* Add filters */}
+                Data filters
+                <DottedSeparator className="my-4" />
+                
+                <>
+                    <TabsContent className="mt-0" value="table">
+                        Data table
+                    </TabsContent>
+                    <TabsContent className="mt-0" value="kanban">
+                        Data kanban
+                    </TabsContent>
+                    <TabsContent className="mt-0" value="calendar">
+                        Data calendar
+                    </TabsContent>
+                </>
+            </div>
+        </Tabs>
+    );
+};
